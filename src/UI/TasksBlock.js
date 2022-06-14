86b7e110baae3ca7068c2f62285d4c0af02f5310
yrsolanaratonmi <romacrying@icloud.com>
2022-06-14 23:58:30 +0300

    чутьчуть стилей накинул

--- src/UI/TasksBlock.js
+++ src/UI/TasksBlock.js
@@ -2,19 +2,23 @@ import styles from './tasksblock.module.css'
 import React from "react";
 
 
+let inputGroup = `${styles.main} + "input-group mb-3"`
+
 let myStyle = `${styles.color} + "list-group-item"`
 
 let StyleForInput = `${styles.aboba} + "form-control"`
+
 
 let TasksBlock = (props) => {
 
     props.info.tasksInfo = props.info.tasksInfo
         .filter(element => !(element.isDeleting))
+
     let CurrentTasks = props.info.tasksInfo
 
     let AllTasks = CurrentTasks
         .map(el => <li className={myStyle}>{el.text}
-            <button className="btn btn-outline-secondary" onClick={()=>{
+            <button className="btn btn-outline-secondary btn-lg" type="button"  onClick={() => {
                 el.isDeleting = true
                 console.log(el.text)
                 console.log(el.isDeleting)
@@ -33,18 +37,21 @@ let TasksBlock = (props) => {
     }
 
 
-
     return (
-        <div className={styles.main}>
+        <div className={inputGroup}>
             <ul className="list-group">
                 {AllTasks}
             </ul>
-            <div>
-                <input ref={a} className={StyleForInput}></input>
-                <button className="btn btn-outline-secondary" type="button" onClick={func}>Создать задачу</button>
+            <div className={inputGroup} >
+                <input ref={a} className={StyleForInput} placeholder="Введите задачу" aria-label = "Recipient's username" aria-describedby = "basic-addon2"></input>
+                <div className='input-group-append'>
+                    <button className="btn btn-outline-secondary btn-lg" type="button" onClick={func}>Создать задачу</button>
+                </div>
+
             </div>
         </div>
     )
 }
 
 export default TasksBlock
+
